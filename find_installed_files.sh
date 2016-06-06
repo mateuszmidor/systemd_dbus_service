0a4704b4d58edd27d58aa7da57d4110a7e12619d
mateuszmidor <kontakt@mateuszmidor.com>
2016-06-06 14:15:53 +0200

    Updated find_installed_files.sh

--- find_installed_files.sh
+++ find_installed_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ARTIFACTS=('libCommonAPI.so*' 'Calc-stub.ini' 'com.mateuszmidor.Calc.conf' 'Calc.service' 'CalcServer')
+ARTIFACTS=('libCommonAPI.so*' 'Calc-stub.ini' 'com.mateuszmidor.calc.calcInterface.conf' 'Calc.service' 'CalcServer')
 PATHS=('/usr' '/etc' '/lib' '/bin')
 
 for path in ${PATHS[@]}; do
